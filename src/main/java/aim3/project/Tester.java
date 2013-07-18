9e8c48cd34090c8aab4f59d5f115cdd8f5789951
UbuntuVM <igorv@ubuntu.(none)>
2013-07-17 17:19:27 -0700

    Initial commit

---
+++ src/main/java/aim3/project/Tester.java
@@ -0,0 +1,286 @@
+package aim3.project;
+
+import java.util.Iterator;
+
+import eu.stratosphere.pact.common.contract.CrossContract;
+import eu.stratosphere.pact.common.contract.FileDataSink;
+import eu.stratosphere.pact.common.contract.FileDataSource;
+import eu.stratosphere.pact.common.contract.MapContract;
+import eu.stratosphere.pact.common.contract.MatchContract;
+import eu.stratosphere.pact.common.contract.ReduceContract;
+import eu.stratosphere.pact.common.contract.ReduceContract.Combinable;
+import eu.stratosphere.pact.common.io.RecordInputFormat;
+import eu.stratosphere.pact.common.io.RecordOutputFormat;
+import eu.stratosphere.pact.common.plan.Plan;
+import eu.stratosphere.pact.common.plan.PlanAssembler;
+import eu.stratosphere.pact.common.plan.PlanAssemblerDescription;
+import eu.stratosphere.pact.common.stubs.Collector;
+import eu.stratosphere.pact.common.stubs.CrossStub;
+import eu.stratosphere.pact.common.stubs.MapStub;
+import eu.stratosphere.pact.common.stubs.MatchStub;
+import eu.stratosphere.pact.common.stubs.ReduceStub;
+import eu.stratosphere.pact.common.stubs.StubAnnotation.ConstantFields;
+import eu.stratosphere.pact.common.stubs.StubAnnotation.ConstantFieldsSecondExcept;
+import eu.stratosphere.pact.common.stubs.StubAnnotation.OutCardBounds;
+import eu.stratosphere.pact.common.type.PactRecord;
+import eu.stratosphere.pact.common.type.base.PactDouble;
+import eu.stratosphere.pact.common.type.base.PactInteger;
+import eu.stratosphere.pact.common.type.base.PactString;
+import eu.stratosphere.pact.common.type.base.parser.DecimalTextDoubleParser;
+import eu.stratosphere.pact.common.type.base.parser.DecimalTextIntParser;
+import eu.stratosphere.pact.common.type.base.parser.VarLengthStringParser;
+import eu.stratosphere.pact.common.util.FieldSet;
+
+public class Tester implements PlanAssemblerDescription, PlanAssembler {
+
+	public Plan getPlan(String... args) {
+		int noSubTasks = (args.length > 0 ? Integer.parseInt(args[0]) : 1);
+		String trainInput = (args.length > 1 ? args[1] : "");
+		String testInput = (args.length > 2 ? args[2] : "");
+		String output = (args.length > 3 ? args[3] : "");
+
+		// Configure the training source:
+
+		FileDataSource train = new FileDataSource(RecordInputFormat.class,
+				trainInput, "Training results");
+		train.setDegreeOfParallelism(noSubTasks);
+		train.getCompilerHints().setUniqueField(new FieldSet(1));
+		RecordInputFormat.configureRecordFormat(train).recordDelimiter('\n')
+				.fieldDelimiter('\t').field(DecimalTextIntParser.class, 0)
+				.field(DecimalTextIntParser.class, 1)
+				.field(DecimalTextDoubleParser.class, 2);
+
+		// Configure the test source:
+		FileDataSource test = new FileDataSource(RecordInputFormat.class,
+				testInput, "Test Data");
+		test.setDegreeOfParallelism(noSubTasks);
+		test.getCompilerHints().setUniqueField(new FieldSet(0));
+		RecordInputFormat.configureRecordFormat(test).recordDelimiter('\n')
+				.fieldDelimiter('\t').field(DecimalTextIntParser.class, 0)
+				.field(DecimalTextIntParser.class, 1)
+				.field(VarLengthStringParser.class, 2)
+				.field(DecimalTextIntParser.class, 3)
+				.field(DecimalTextIntParser.class, 4);
+
+		MapContract processTrain = MapContract.builder(ProcessTrainData.class)
+				.input(train).name("Filter the training data").build();
+		processTrain.setDegreeOfParallelism(noSubTasks);
+		processTrain.getCompilerHints().setAvgRecordsEmittedPerStubCall(1.0f);
+		processTrain.getCompilerHints().setAvgBytesPerRecord(16);
+
+		MapContract processTest = MapContract.builder(ProcessTestData.class)
+				.input(test).name("Filter the test data").build();
+		processTest.setDegreeOfParallelism(noSubTasks);
+		processTrain.getCompilerHints().setAvgBytesPerRecord(120);
+		processTrain.getCompilerHints().setUniqueField(
+				new FieldSet(new int[] { 0 }));
+
+		MatchContract joinTrain = MatchContract
+				.builder(JoinTrain.class, PactInteger.class, 1, 0)
+				.input1(processTrain).input2(processTest)
+				.name("Get the messages for the training data").build();
+		joinTrain.setDegreeOfParallelism(noSubTasks);
+
+		/*
+		
+		MatchContract joinTrainAndTest = MatchContract
+				.builder(JoinTrainAndTest.class, PactInteger.class, 0, 3)
+				.input1(joinTrain).input2(processTest)
+				.name("Determine K(train_i, test_j)").build();
+		joinTrainAndTest.setDegreeOfParallelism(noSubTasks);
+
+		*/
+		
+		CrossContract joinTrainAndTest = CrossContract
+				.builder(CrossTrainAndTrest.class)
+				.input1(joinTrain).input2(processTest)
+				.name("Determine K(train_i, test_j)").build();
+		joinTrainAndTest.setDegreeOfParallelism(noSubTasks);
+
+		ReduceContract reduceByID = ReduceContract
+				.builder(ReduceForID.class, PactInteger.class, 1)
+				.input(joinTrainAndTest).name("Aggregate sum for every test")
+				.build();
+		reduceByID.setDegreeOfParallelism(noSubTasks);
+
+		ReduceContract reduceByTrain = ReduceContract
+				.builder(ReduceForTrain.class, PactInteger.class, 0)
+				.input(reduceByID).name("Determine accuracy for every testval")
+				.build();
+
+		reduceByTrain.setDegreeOfParallelism(noSubTasks);
+
+		FileDataSink out = new FileDataSink(RecordOutputFormat.class, output,
+				reduceByTrain, "Group accuracy");
+
+		RecordOutputFormat.configureRecordFormat(out).recordDelimiter('\n')
+				.fieldDelimiter('\t').lenient(true).field(PactInteger.class, 0)
+				.field(PactInteger.class, 1).field(PactInteger.class, 2);
+
+		Plan plan = new Plan(out, "SVM Tester");
+		plan.setDefaultParallelism(noSubTasks);
+
+		return plan;
+	}
+
+	public String getDescription() {
+		return "Parameters: [noSubTasks] [input:TrainData] [input:TestData] [output]";
+	}
+
+	@ConstantFields(fields = { 0, 1, 2, 3, 4 })
+	@OutCardBounds(lowerBound = 0, upperBound = 1)
+	public static class ProcessTestData extends MapStub {
+
+		@Override
+		public void map(PactRecord record, Collector<PactRecord> collector)
+				throws Exception {
+			PactInteger p = record.getField(4, PactInteger.class);
+			if (p.getValue() > 0)
+				collector.collect(record);
+
+		}
+
+	}
+
+	@ConstantFields(fields = { 0, 1, 2 })
+	public static class ProcessTrainData extends MapStub {
+
+		@Override
+		public void map(PactRecord record, Collector<PactRecord> collector)
+				throws Exception {
+			collector.collect(record);
+
+		}
+
+	}
+
+	@ConstantFieldsSecondExcept(fields = { 4 })
+	@OutCardBounds(lowerBound = 1, upperBound = 1)
+	public static class JoinTrain extends MatchStub {
+		@Override
+		public void match(PactRecord first, PactRecord second,
+				Collector<PactRecord> collector) throws Exception {
+			second.setField(4, first.getField(2, PactDouble.class));
+			collector.collect(second);
+
+		}
+	}
+
+	public static class JoinTrainAndTest extends MatchStub {
+		PactRecord rec = new PactRecord(4);
+
+		@Override
+		public void match(PactRecord first, PactRecord second,
+				Collector<PactRecord> collector) throws Exception {
+			// TODO Auto-generated method stub
+			NGramKernel train = new NGramKernel(2, 6, 3);
+			double v = train.calculate(first.getField(2, PactString.class)
+					.toString(), second.getField(2, PactString.class)
+					.toString());
+			v = v * first.getField(4, PactDouble.class).getValue()*first.getField(1, PactInteger.class).getValue();
+			// TrainVal, ID, sent, val
+
+			rec.setField(0, first.getField(3, PactInteger.class));
+			rec.setField(1, new PactInteger(second.getField(0, PactInteger.class).getValue()+first.getField(3, PactInteger.class).getValue()*8000));
+			rec.setField(2, second.getField(1, PactInteger.class));
+			rec.setField(3, new PactDouble(v));
+			collector.collect(rec);
+		}
+	}
+
+	public static class CrossTrainAndTrest extends CrossStub {
+		PactRecord rec = new PactRecord(4);
+
+		@Override
+		public void cross(PactRecord first, PactRecord second,
+				Collector<PactRecord> collector) {
+			// TODO Auto-generated method stub
+			if (first.getField(3, PactInteger.class).getValue() != second
+					.getField(3, PactInteger.class).getValue()) {
+				NGramKernel train = new NGramKernel(2, 6, 3);
+				double v = train.calculate(first.getField(2, PactString.class)
+						.toString(), second.getField(2, PactString.class)
+						.toString());
+				v = v * first.getField(4, PactDouble.class).getValue()*first.getField(1, PactInteger.class).getValue();
+				// TrainVal, ID, sent, val
+
+				rec.setField(0, first.getField(3, PactInteger.class));
+				rec.setField(1, new PactInteger(second.getField(0, PactInteger.class).getValue()+first.getField(3, PactInteger.class).getValue()*8000));
+				rec.setField(2, second.getField(1, PactInteger.class));
+				rec.setField(3, new PactDouble(v));
+				collector.collect(rec);
+			}
+		}
+
+	}
+
+	@ConstantFields(fields = { 0 })
+	@OutCardBounds(lowerBound = 1, upperBound = 1)
+	public static class ReduceForID extends ReduceStub {
+		PactRecord rec;
+		PactInteger one = new PactInteger(1);
+		PactInteger zero = new PactInteger(0);
+		PactRecord out = new PactRecord(3);
+
+		@Override
+		public void reduce(Iterator<PactRecord> iterator,
+				Collector<PactRecord> collector) throws Exception {
+			double sum = 0;
+			int sent = 0;
+			PactInteger train = null;
+			while (iterator.hasNext()) {
+				rec = iterator.next();
+				if (train == null)
+					train = rec.getField(0, PactInteger.class);
+				if (sent == 0)
+					sent = rec.getField(2, PactInteger.class).getValue();
+				sum = sum + rec.getField(3, PactDouble.class).getValue();
+			}
+			out.setField(0, train);
+			if (sum * sent > 0) {
+				out.setField(1, this.one);
+				out.setField(2, this.zero);
+			} else {
+				out.setField(1, this.zero);
+				out.setField(2, this.one);
+			}
+			collector.collect(out);
+		}
+
+	}
+
+	@ConstantFields(fields = { 0 })
+	@OutCardBounds(lowerBound = 1, upperBound = 1)
+	@Combinable
+	public static class ReduceForTrain extends ReduceStub {
+		PactRecord rec;
+		PactRecord out = new PactRecord(3);
+
+		@Override
+		public void reduce(Iterator<PactRecord> iterator,
+				Collector<PactRecord> collector) throws Exception {
+			int sumGood = 0;
+			int sumBad = 0;
+			PactInteger train = null;
+
+			while (iterator.hasNext()) {
+				rec = iterator.next();
+				if (train == null)
+					train = rec.getField(0, PactInteger.class);
+				sumGood += rec.getField(1, PactInteger.class).getValue();
+				sumBad += rec.getField(2, PactInteger.class).getValue();
+			}
+			out.setField(0, train);
+			out.setField(1, new PactInteger(sumGood));
+			out.setField(2, new PactInteger(sumBad));
+			collector.collect(out);
+		}
+
+		@Override
+		public void combine(Iterator<PactRecord> iterator,
+				Collector<PactRecord> collector) throws Exception {
+			this.reduce(iterator, collector);
+		}
+	}
+
+}
---
+++ src/main/java/aim3/project/Trainer.java
@@ -0,0 +1,449 @@
+package aim3.project;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import com.joptimizer.functions.ConvexMultivariateRealFunction;
+import com.joptimizer.functions.LinearMultivariateRealFunction;
+import com.joptimizer.functions.PDQuadraticMultivariateRealFunction;
+import com.joptimizer.optimizers.JOptimizer;
+import com.joptimizer.optimizers.OptimizationRequest;
+import com.numericalmethod.suanshu.algebra.linear.matrix.doubles.Matrix;
+import com.numericalmethod.suanshu.algebra.linear.matrix.doubles.matrixtype.dense.DenseMatrix;
+import com.numericalmethod.suanshu.algebra.linear.matrix.doubles.matrixtype.sparse.SparseVector;
+import com.numericalmethod.suanshu.algebra.linear.vector.doubles.Vector;
+import com.numericalmethod.suanshu.algebra.linear.vector.doubles.dense.DenseVector;
+import com.numericalmethod.suanshu.analysis.function.rn2r1.QuadraticFunction;
+import com.numericalmethod.suanshu.graph.type.SparseDiGraph;
+import com.numericalmethod.suanshu.misc.PrecisionUtils;
+import com.numericalmethod.suanshu.optimization.multivariate.constrained.constraint.linear.LinearEqualityConstraints;
+import com.numericalmethod.suanshu.optimization.multivariate.constrained.constraint.linear.LinearGreaterThanConstraints;
+import com.numericalmethod.suanshu.optimization.multivariate.constrained.convex.sdp.socp.qp.QPSolution;
+import com.numericalmethod.suanshu.optimization.multivariate.constrained.convex.sdp.socp.qp.activeset.QPPrimalActiveSetSolver;
+import com.numericalmethod.suanshu.optimization.multivariate.constrained.convex.sdp.socp.qp.activeset.QPPrimalActiveSetSolver.Solution;
+import com.numericalmethod.suanshu.optimization.multivariate.constrained.convex.sdp.socp.qp.problem.QPProblem;
+
+import eu.stratosphere.pact.common.contract.FileDataSink;
+import eu.stratosphere.pact.common.contract.FileDataSource;
+import eu.stratosphere.pact.common.contract.MapContract;
+import eu.stratosphere.pact.common.contract.MatchContract;
+import eu.stratosphere.pact.common.contract.ReduceContract;
+import eu.stratosphere.pact.common.io.RecordInputFormat;
+import eu.stratosphere.pact.common.io.RecordOutputFormat;
+import eu.stratosphere.pact.common.plan.Plan;
+import eu.stratosphere.pact.common.plan.PlanAssembler;
+import eu.stratosphere.pact.common.plan.PlanAssemblerDescription;
+import eu.stratosphere.pact.common.stubs.Collector;
+import eu.stratosphere.pact.common.stubs.MapStub;
+import eu.stratosphere.pact.common.stubs.MatchStub;
+import eu.stratosphere.pact.common.stubs.ReduceStub;
+import eu.stratosphere.pact.common.stubs.StubAnnotation.ConstantFields;
+import eu.stratosphere.pact.common.stubs.StubAnnotation.ConstantFieldsFirst;
+import eu.stratosphere.pact.common.stubs.StubAnnotation.ConstantFieldsSecond;
+import eu.stratosphere.pact.common.stubs.StubAnnotation.OutCardBounds;
+import eu.stratosphere.pact.common.type.PactRecord;
+import eu.stratosphere.pact.common.type.base.PactDouble;
+import eu.stratosphere.pact.common.type.base.PactInteger;
+import eu.stratosphere.pact.common.type.base.PactMap;
+import eu.stratosphere.pact.common.type.base.PactString;
+import eu.stratosphere.pact.common.type.base.parser.DecimalTextDoubleParser;
+import eu.stratosphere.pact.common.type.base.parser.DecimalTextIntParser;
+import eu.stratosphere.pact.common.type.base.parser.VarLengthStringParser;
+import eu.stratosphere.pact.common.util.FieldSet;
+
+
+public class Trainer implements PlanAssembler, PlanAssemblerDescription {
+
+	public String getDescription() {
+		return "Parameters: [noSubTasks] [input] [output]";
+	}
+
+	public Plan getPlan(String... args) {
+		int noSubTasks = (args.length > 0 ? Integer.parseInt(args[0]) : 1);
+		String trainData = (args.length > 1 ? args[1] : "");
+		String output = (args.length > 2 ? args[2] : "");
+
+		// Configure the training source:
+
+		FileDataSource train = new FileDataSource(RecordInputFormat.class,
+				trainData, "Training Data");
+		train.setDegreeOfParallelism(noSubTasks);
+		train.getCompilerHints().setUniqueField(new FieldSet(0));
+		RecordInputFormat.configureRecordFormat(train).recordDelimiter('\n')
+				.fieldDelimiter('\t').field(DecimalTextIntParser.class, 0)
+				.field(DecimalTextIntParser.class, 1)
+				.field(VarLengthStringParser.class, 2)
+				.field(DecimalTextIntParser.class, 3)
+				.field(DecimalTextIntParser.class, 4);
+
+		MapContract processTrain = MapContract.builder(ProcessInput.class)
+				.input(train).name("Filter the training data").build();
+		processTrain.setDegreeOfParallelism(noSubTasks);
+		processTrain.getCompilerHints().setAvgRecordsEmittedPerStubCall(0.1f);
+		processTrain.getCompilerHints().setAvgBytesPerRecord(100);
+
+		MatchContract joinTrain = MatchContract
+				.builder(CreatePairs.class, PactInteger.class, 3, 3)
+				.input1(processTrain).input2(processTrain)
+				.name("Determine similarities for training").build();
+		joinTrain.setDegreeOfParallelism(noSubTasks);
+
+		ReduceContract trainSVMs = ReduceContract
+				.builder(ReduceTrainer.class, PactInteger.class, 0)
+				.input(joinTrain).name("Reduce and feed into the optimizer")
+				.build();
+		trainSVMs.setDegreeOfParallelism(noSubTasks);
+
+		FileDataSink out = new FileDataSink(RecordOutputFormat.class, output,
+				trainSVMs, "Training results");
+
+		RecordOutputFormat.configureRecordFormat(out).recordDelimiter('\n')
+				.fieldDelimiter('\t').lenient(true).field(PactInteger.class, 0)
+				.field(PactInteger.class, 1).field(PactDouble.class, 2);
+
+		/*
+		 * FileDataSink out = new FileDataSink(RecordOutputFormat.class,
+		 * output,joinTrain, "The matrices");
+		 * RecordOutputFormat.configureRecordFormat
+		 * (out).recordDelimiter('\n').fieldDelimiter
+		 * ('\t').lenient(true).field(PactInteger.class,
+		 * 0).field(PactInteger.class, 1). field(PactInteger.class,
+		 * 2).field(PactInteger.class, 3).field(PactInteger.class,
+		 * 4).field(PactInteger.class, 5);
+		 */
+
+		Plan plan = new Plan(out, "SVM Trainer");
+		plan.setDefaultParallelism(noSubTasks);
+
+		return plan;
+	}
+
+	/**
+	 * Processes the input PactRecord to only include the data we need. The
+	 * output will be: ID, Sentiment, Message, TrainID
+	 * 
+	 * @author Igor Viskovic: igorv@mailbox.tu-berlin.de
+	 * 
+	 */
+	@ConstantFields(fields = { 0, 1, 2, 3 })
+	@OutCardBounds(lowerBound = 0, upperBound = 1)
+	public static class ProcessInput extends MapStub {
+
+		@Override
+		public void map(PactRecord record, Collector<PactRecord> collector)
+				throws Exception {
+			record.setNull(4);
+			PactInteger p = record.getField(3, PactInteger.class);
+			if (p.getValue() > 0)
+				collector.collect(record);
+			// Format: ID, Sent, Mes, TrainVal
+		}
+
+	}
+
+	@ConstantFieldsFirst(fields = { 0, 3 })
+	@ConstantFieldsSecond(fields = { 0, 1, 3 })
+	@OutCardBounds(lowerBound = 0, upperBound = 1)
+	public static class CreatePairs extends MatchStub {
+		@Override
+		public void match(PactRecord first, PactRecord second,
+				Collector<PactRecord> collector) throws Exception {
+			// TODO Auto-generated method stub
+
+			// First check if first.ID<=second.ID. We use the symmetry of the
+			// matrix
+
+		//	if (first.getField(0, PactInteger.class).getValue() <= second
+		//			.getField(0, PactInteger.class).getValue()) {
+
+				PactRecord out = new PactRecord(5);
+				out.setField(0, first.getField(3, PactInteger.class));
+				// The training value
+
+				out.setField(2, first.getField(0, PactInteger.class));
+				// The ID of the first element
+
+				out.setField(3, second.getField(0, PactInteger.class));
+				// The ID of the second element
+
+				NGramKernel train = new NGramKernel(2, 6, 1);
+				int v = train.calculate(first.getField(2, PactString.class)
+						.toString(), second.getField(2, PactString.class)
+						.toString());
+
+				v = v * first.getField(1, PactInteger.class).getValue();
+
+				v = v * second.getField(1, PactInteger.class).getValue();
+
+				// We save the sentiments directly in v
+
+				PactInteger i = new PactInteger(v);
+
+				out.setField(1, i);
+
+				out.setField(4, first.getField(1, PactInteger.class)); //
+				// Sentiment of the first element
+				out.setField(5, second.getField(1, PactInteger.class)); //
+				// Sentiment of the second element
+				collector.collect(out);
+			}
+		//}
+
+	}
+
+	public static class ReduceTrainer extends ReduceStub {
+
+		@Override
+		public void reduce(Iterator<PactRecord> values,
+				Collector<PactRecord> collector) throws Exception {
+			int size = 0;
+			PactRecord rec = null;
+
+			// We don't know how many training values we're gonna get, so we
+			// store them in hash maps first
+
+			HashMap<Integer, Integer> idToIndex = new HashMap<Integer, Integer>();
+			// HashMap<Integer, Integer> indexToID=new HashMap<Integer,
+			// Integer>();
+
+			int trainval = 0;
+			int firstID, secondID, val;
+
+			HashMap<Integer, Integer> matrix = new HashMap<Integer, Integer>();
+			HashMap<Integer, Integer> array = new HashMap<Integer, Integer>();
+
+			int firstInd = 0, secondInd = 0;
+
+			while (values.hasNext()) {
+				rec = values.next();
+				firstID = rec.getField(2, PactInteger.class).getValue();
+				secondID = rec.getField(3, PactInteger.class).getValue();
+				val = rec.getField(1, PactInteger.class).getValue();
+
+				// Determine an index for the first ID if necessary
+				if (!idToIndex.containsKey(firstID)) {
+					idToIndex.put(firstID, size);
+					firstInd= size++;
+				} else {
+					firstInd = idToIndex.get(firstID);
+				}
+
+				// Determine an index for the second ID if necessary
+				if (!idToIndex.containsKey(secondID)) {
+					idToIndex.put(secondID, size);
+					secondInd=size++;
+				} else {
+					secondInd = idToIndex.get(secondID);
+				}
+
+				// Add the value to the matrix. We shouldn't encounter
+				// duplicates, but we want to be sure
+				if (!matrix.containsKey(firstInd * 10000 + secondInd)) {
+					matrix.put(firstInd * 10000 + secondInd, val);
+				}
+
+				// Add the sentiment to the array, this is later used for the
+				// bounds
+				if (!array.containsKey(firstInd)) {
+					array.put(firstInd, rec.getField(4, PactInteger.class)
+							.getValue());
+				}
+
+				// Add the sentiment if necessary
+				if (!array.containsKey(secondInd)) {
+					array.put(secondInd, rec.getField(5, PactInteger.class)
+							.getValue());
+				}
+			}
+
+			// We now know the size of the matrix and can proceed to create it
+			// and fill it
+
+			double[][] mat = new double[size][size];
+
+			Iterator it = matrix.entrySet().iterator();
+
+			while (it.hasNext()) {
+				Map.Entry<Integer, Integer> pairs = (Entry<Integer, Integer>) it
+						.next();
+				firstInd = pairs.getKey() / 10000;
+				secondInd = pairs.getKey() % 10000;
+				mat[firstInd][secondInd] = (double) pairs.getValue();
+				mat[secondInd][firstInd] = mat[firstInd][secondInd];
+			/*	rec=new PactRecord(3);
+				rec.setField(2, new PactDouble(mat[firstInd][secondInd]));
+				rec.setField(0, new PactInteger(firstInd));
+				rec.setField(1, new PactInteger(secondInd));
+				collector.collect(rec); */
+			}
+
+			// Now mat contains all the values we'll need
+
+			// This is the constraint that the sum of alpha_i * y_i is 0
+			double[][] A = new double[1][size];
+
+			// This is part of the goal function as SUM_{1,n} alpha_i . It
+			// contains only -1, because the original problem is a maximization
+			// problem and we have access to a minimizer
+			double[] q = new double[size];
+			for (int i = 0; i < size; i++) {
+				q[i] = -1;
+			}
+
+			it = array.entrySet().iterator();
+			while (it.hasNext()) {
+				Map.Entry<Integer, Integer> pairs = (Entry<Integer, Integer>) it
+						.next();
+				A[0][pairs.getKey()] = (double)pairs.getValue();
+			}
+			
+			Matrix H1 = new DenseMatrix(mat);
+			Vector p = new DenseVector(q);
+			
+			QuadraticFunction F = new QuadraticFunction(H1, p);
+
+			Matrix Adense = new DenseMatrix(A);
+			Vector b = new DenseVector(new double[]{0});
+			LinearEqualityConstraints EQUALITY = new LinearEqualityConstraints(Adense, b);
+			
+			double[][] ineq = new double[2 * size][size];
+			double []bound = new double[2*size];
+			for(int i=0;i<size;i++){
+				//Takes care of the lower bound >=0
+				ineq[i][i]=1;
+				bound[i]=0;
+				
+				
+				//Take care of the upper bound
+				ineq[size+i][i]=-1;
+				bound[i+size]=3000;
+				
+			}
+			
+			Matrix ineqDense = new DenseMatrix(ineq);
+			Vector boundDense = new DenseVector(bound);
+			LinearGreaterThanConstraints GREATER = new LinearGreaterThanConstraints(ineqDense, boundDense);
+			
+			QPProblem problem = new QPProblem(F, EQUALITY, GREATER, null);
+			QPPrimalActiveSetSolver instance = new QPPrimalActiveSetSolver(PrecisionUtils.autoEpsilon(H1), 100000);
+			Solution soln = instance.solve(problem);
+			QPSolution qpSoln = soln.search();
+			double[] result=qpSoln.minimizer().toArray();
+			
+		//	ConvexMultivariateRealFunction[] inequalities = new ConvexMultivariateRealFunction[2 * size];
+			
+			/*for (int i = 0; i < size; i++) {
+				//Takes care of the lower bound >=0
+				ine[i]=1.0;
+				inequalities[i]=new LinearMultivariateRealFunction(ine, 0.0);
+				
+				// Takes care of the upper bound (<=C)
+				ine[i]=-1.0;
+				inequalities[i+size]=new LinearMultivariateRealFunction(ine, 10000.0);
+				
+				
+				/*ineq[i][i] = -1;
+				inequalities[i] = new LinearMultivariateRealFunction(ineq[i],
+						1000);
+				ineq[i + size][i] = 1;*/
+				// Takes care of the lower bound
+				/*inequalities[i + size] = new LinearMultivariateRealFunction(
+						ineq[i + size], 0);*/
+				
+			//}
+
+			//double[] result;
+			// try {
+			/*PDQuadraticMultivariateRealFunction objectiveFunction = new PDQuadraticMultivariateRealFunction(
+					mat, q, 0);*/
+			
+			/*
+			
+			for(int i=0;i<size;i++){
+				for(int j=i;j<size;j++){
+					rec=new PactRecord(3);
+					rec.setField(2, new PactDouble(mat[i][j]));
+					rec.setField(0, new PactInteger(i));
+					rec.setField(1, new PactInteger(j));
+					collector.collect(rec);
+				}
+			}
+			
+			for(int i=0;i<size;i++){
+				rec=new PactRecord(3);
+				rec.setField(2, new PactDouble(A[0][i]));
+				rec.setField(0, new PactInteger(i));
+				rec.setField(1, new PactInteger(1000));
+				collector.collect(rec);
+				
+				
+			}*/
+		/*	for(int i=0;i<2*size;i++){
+				rec=new PactRecord(3);
+				if(i<size)
+					rec.setField(2, new PactDouble(10000));
+				else rec.setField(2, new PactDouble(0));
+				rec.setField(0, new PactInteger(i));
+				rec.setField(1, new PactInteger((int)ineq[i][i%size]));
+				collector.collect(rec);
+			}
+			*/
+			
+			
+			/*
+			//Set the objective function: minimize 0.5*xT*mat*x + qx + 0 <-> maximize -qx + 0.5*xT*mat*X +0
+			OptimizationRequest or = new OptimizationRequest();
+			or.setF0(objectiveFunction);
+			// Set the inequalities
+			//or.setFi(inequalities);
+			//or.setInitialPoint(new double[size]);
+			
+			or.setNotFeasibleInitialPoint(new double[size]);
+			
+			//Array for SUM_{1,n} alpha_i * y_i
+			or.setA(A);
+			
+			
+			
+			//B is 0 for the constraint SUM a_i*y_i = 0
+			double[] b=new double[1];
+			b[0]=0.0;
+			or.setB(b);
+			//or.setToleranceFeas(1.E-3);
+			//or.setTolerance(1.E-3);
+
+			JOptimizer opt = new JOptimizer();
+			opt.setOptimizationRequest(or);
+			int returnCode = opt.optimize();
+
+			result = opt.getOptimizationResponse().getSolution();
+			
+			*/
+			rec = new PactRecord(3);
+
+			it = idToIndex.entrySet().iterator();
+			while (it.hasNext()) {
+				Map.Entry<Integer, Integer> pairs = (Entry<Integer, Integer>) it
+						.next();
+				firstID = pairs.getKey();
+				if (result[pairs.getValue()] > 0)
+					rec.setField(0, new PactInteger(trainval));
+				rec.setField(1, new PactInteger(firstID));
+				rec.setField(2, new PactDouble(result[pairs.getKey()]));
+				collector.collect(rec);
+			}
+		
+			/*
+			 * } catch (Exception e) { for (int i = 0; i < size; i++) { if (i %
+			 * 5 == 0) { // firstID = (trainval - 1) * size + i + 1; firstID = i
+			 * * 80 + trainval; rec.setField(0, new PactInteger(trainval));
+			 * rec.setField(1, new PactInteger(firstID)); rec.setField(2, new
+			 * PactDouble(Math.random())); collector.collect(rec); } } }
+			 */
+
+		}
+	}
+
+}
